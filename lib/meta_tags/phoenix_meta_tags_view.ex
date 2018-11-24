20ac61985bd69450dc9a7c2e4833bf95a4cdccd8
long <hlongvu@gmail.com>
2018-11-24 23:12:57 +0700

    render other tags by defining inside a map

--- lib/meta_tags/phoenix_meta_tags_view.ex
+++ lib/meta_tags/phoenix_meta_tags_view.ex
@@ -4,9 +4,15 @@ defmodule PhoenixMetaTags.TagView do
   This module render the tags struct to html meta tag.
   """
 
-
   defmacro __using__(_) do
     quote do
+
+
+      @default_tags [:title, :description, :image, :url]
+
+      defp is_default_tag(t) do
+        Enum.member?(@default_tags, t) == true
+      end
 
       defp get_value(tags, item) do
         tags[item] || Application.get_env(:phoenix_meta_tags , item)
@@ -48,6 +54,34 @@ defmodule PhoenixMetaTags.TagView do
           tag(:meta, content: get_value(tags,:image),  property: "twitter:image")
         ]
       end
+
+
+      def render_tags_other(tags) do
+          tags
+          |> Map.keys()
+          |> Enum.filter(fn x -> !is_default_tag(x) end)
+          |> Enum.map( fn x -> {x, get_value(tags, x)} end)
+          |> Map.new()
+          |> render_o_tags()
+      end
+
+      defp render_o_tags(other_tags) do
+        other_tags
+        |> Map.keys()
+        |> Enum.map(fn x -> render_o_element(x, get_value(other_tags, x)) end)
+        |> List.flatten()
+      end
+
+      defp render_o_element(key, value) when is_map(value) do
+         value
+         |> Map.keys()
+         |> Enum.map(fn x -> tag(:meta, content: value[x],  property: Atom.to_string(key) <> ":" <> Atom.to_string(x)  ) end)
+      end
+
+      defp render_o_element(key, value) do
+        [tag(:meta, content: value,  property: Atom.to_string(key))]
+      end
+
 
       @doc """
         Render all meta tags for default, open graph and twitter
--- test/phoenix_meta_tags_test.exs
+++ test/phoenix_meta_tags_test.exs
@@ -1,8 +1,58 @@
 defmodule PhoenixMetaTagsTest do
   use ExUnit.Case
   doctest PhoenixMetaTags
+  use Phoenix.HTML
+  use PhoenixMetaTags.TagView
 
-  test "greets the world" do
-    assert PhoenixMetaTags.hello() == :world
+
+  test "only default tags" do
+
+    tags = %{
+      title: "hlongvu;",
+      description: "Hlongvu Blog",
+      url: "https://blog.hlongvu.com",
+      image: "https://images.unsplash.com/photo-1519337265831-281ec6cc8514?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e0604c8783c88d26ff2b89c3fc0ac137&auto=format&fit=crop&w=1500&q=80"
+    }
+
+    other_tags = render_tags_other(tags)
+    assert other_tags == []
   end
+
+  test "render other tags with key value" do
+    tags = %{
+      title: "hlongvu;",
+      title1: "hlongvu1;",
+      title2: "hlongvu2;",
+    }
+
+    result1 = tag(:meta, content: "hlongvu1;",  property: "title1")
+    result2 = tag(:meta, content: "hlongvu2;",  property: "title2")
+
+    assert render_tags_other(tags) == [result1 ,result2]
+
+  end
+
+  test "render other tags with map" do
+
+    tags = %{
+      title: "hlongvu;",
+      description: "Hlongvu Blog",
+      url: "https://blog.hlongvu.com",
+      image: "https://images.unsplash.com/photo-1519337265831-281ec6cc8514?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e0604c8783c88d26ff2b89c3fc0ac137&auto=format&fit=crop&w=1500&q=80",
+      other_url: "https://blog.otherurl.com",
+      fb: %{appid: "1200129192192192"}
+    }
+
+
+    result1 = tag(:meta, content: "https://blog.otherurl.com",  property: "other_url")
+    result2 = tag(:meta, content: "1200129192192192",  property: "fb:appid")
+
+
+    other_tags = render_tags_other(tags)
+    assert other_tags == [result2, result1]
+
+  end
+
+
+
 end
